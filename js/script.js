c4f1c55ad87bf920279b58cd9939786d5f35dfdd
Dimkpa-Daniel <danieldimkpa1@gmail.com>
2022-01-28 13:29:26 +0100

    done with the ourcourses page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,67 @@
+// window.location.href = "courses.html";
+
+// function blocklyFunction() {
+//   location.replace.document.getElementById('blockly')
+  
+// }
+
 
 
 function myFunction() {
-    location.replace("#scratch")
-  }
+  location.replace("courses.html");
+  
+}
+
+function mySignUp(){
+  location.replace("Theform/form.html")
+}
+
+
+function blocklyFunction() {
+  location.replace("courses.html#scratch");
+  
+}
+function dataFunction() {
+  location.replace("courses.html#robotics");
+  
+}
+function webFunction() {
+  location.replace("courses.html#Python");
+  
+}
+function dataFunction() {
+  location.replace("courses.html#Web-Development");
+  
+}
+function embeddedFunction() {
+  location.replace("courses.html#Data-Science");
+  
+}
+function graphicsFunction() {
+  location.replace("courses.html#Graphic-Design");
+  
+}
+function uiFunction() {
+  location.replace("courses.html#UI-UX");
+  
+}
+function pythonFunction() {
+  location.replace("courses.html#Embedded-Systems");
+  
+}
+function roboticsFunction() {
+  location.replace("courses.html#Embedded-Systems");
+  
+}
+function myReviewFunction() {
+  location.replace("review.html");
+  
+}
+
+  
+
+
+
+// function Redirect(){
+//   window.location='courses.html';
+// }
