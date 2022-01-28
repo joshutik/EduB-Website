d62e8871c41ef323d41f5c356cbcfbb30cd61990
Dimkpa-Daniel <danieldimkpa1@gmail.com>
2022-01-28 17:46:14 +0100

    added the review page

--- js/script.js
+++ js/script.js
@@ -17,46 +17,46 @@ function mySignUp(){
 }
 
 
-function blocklyFunction() {
-  location.replace("courses.html#scratch");
+ function blocklyFunction() {
+   location.replace("courses.html#blockly");
   
-}
-function dataFunction() {
-  location.replace("courses.html#robotics");
+ }
+// function dataFunction() {
+//   location.replace("courses.html#robotics");
   
-}
-function webFunction() {
-  location.replace("courses.html#Python");
+// }
+// function webFunction() {
+//   location.replace("courses.html#Python");
   
-}
-function dataFunction() {
-  location.replace("courses.html#Web-Development");
+// }
+// function dataFunction() {
+//   location.replace("courses.html#Web-Development");
   
-}
-function embeddedFunction() {
-  location.replace("courses.html#Data-Science");
+// }
+// function embeddedFunction() {
+//   location.replace("courses.html#Data-Science");
   
-}
-function graphicsFunction() {
-  location.replace("courses.html#Graphic-Design");
+// }
+// function graphicsFunction() {
+//   location.replace("courses.html#Graphic-Design");
   
-}
-function uiFunction() {
-  location.replace("courses.html#UI-UX");
+// }
+// function uiFunction() {
+//   location.replace("courses.html#UI-UX");
   
-}
-function pythonFunction() {
-  location.replace("courses.html#Embedded-Systems");
+// }
+// function pythonFunction() {
+//   location.replace("courses.html#Embedded-Systems");
   
-}
-function roboticsFunction() {
-  location.replace("courses.html#Embedded-Systems");
+// }
+// function roboticsFunction() {
+//   location.replace("courses.html#Embedded-Systems");
   
-}
-function myReviewFunction() {
-  location.replace("review.html");
+// }
+// function myReviewFunction() {
+//   location.replace("review.html");
   
-}
+// }
 
   
 
